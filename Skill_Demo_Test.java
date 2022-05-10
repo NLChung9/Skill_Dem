877028a57022ee238ed1987c882530ee2b4bd3e4
NLChung9 <88344454+NLChung9@users.noreply.github.com>
2022-05-09 20:36:18 -0700

    Update Skill_Demo_Test.java

--- Skill_Demo_Test.java
+++ Skill_Demo_Test.java
@@ -1,9 +1,12 @@
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class Skill_Demo_Test {
     @Test
     public void addition() {
-        assertEquals(999, Skill_Demo.multiply(2,3));
+        assertEquals(6, Skill_Demo.multiply(2,3));
     }
 }
